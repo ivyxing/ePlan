54563780dce1cf90ca091c5a8c3a11b28c5ed5c3
Ivy Xing <mxing@bowdoin.edu>
2014-10-15 15:25:20 -0700

    more npm installed

--- serverNode.js
+++ serverNode.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var mongo = require('mongodb');
+var bodyParser = require('body-parser');
+var mongoskin = require('mongoskin');
 
 var app = express();
-app.use(logfmt.requestLogger());
+app.use(bodyParser());
 
 var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
