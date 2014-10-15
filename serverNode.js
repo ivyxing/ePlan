f9c11039708d57d0a59b250c3eb0201851b878cf
Ivy Xing <mxing@bowdoin.edu>
2014-10-15 15:10:18 -0700

    installed mongo

--- serverNode.js
+++ serverNode.js
@@ -16,6 +16,10 @@ mongo.Db.connect(mongoUri, function(err, db) {
   });
 });
 
+app.get('/', function(req, res) {
+  res.send('anything')
+})
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
